0630c004ab3d1ff832e8f7957e60585dd6061c15
DAVZ2909 <diegoandre09@hotmail.com>
2019-11-20 08:28:41 -0500

    show

---
+++ pregunta3.py
@@ -0,0 +1,52 @@
+import os
+os.system("cls")
+print("¡ CURSO DE NATACION !")
+
+
+
+alumnos = [a, b]
+#alumno 1
+a = int(input("Ingrese al primer alumno: "))
+sexo = int(input("Ingrese el sexo del alumno: "))
+if sexo is H or M:
+    pass
+edad = float(input("Ingrese edad del alumno: "))
+
+if edad (>= 5 and <= 17):
+    print()
+
+#alumno 2
+b = int(input("Ingrese al segundo alumno: "))
+sexo = int(input("Ingrese el sexo del alumno: "))
+if sexo is H or M:
+    pass
+edad = int(input("Ingrese edad del alumno: "))
+if edad (>= 5 and <= 17):
+            print()
+
+#Alumno 3
+b = int(input("Ingrese al tercer alumno: "))
+sexo = int(input("Ingrese el sexo del alumno: "))
+if sexo is H or M:
+    pass
+edad = int(input("Ingrese edad del alumno: "))
+if edad (>= 5 and <= 17):
+            print()
+
+#Alumno 4
+b = int(input("Ingrese al cuarto alumno: "))
+sexo = int(input("Ingrese el sexo del alumno: "))
+if sexo is H or M:
+    pass
+edad = int(input("Ingrese edad del alumno: "))
+if edad (>= 5 and <= 17):
+            print()
+
+#Alumno 5
+b = int(input("Ingrese al quinto alumno: "))
+sexo = int(input("Ingrese el sexo del alumno: "))
+if sexo is H or M:
+    pass
+edad = int(input("Ingrese edad del alumno: "))
+if edad (>= 5 and <= 17):
+            print()
