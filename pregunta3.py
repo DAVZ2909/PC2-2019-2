9846bec5b2ebbe04e8aed01e3a092af43228aa58
DAVZ2909 <diegoandre09@hotmail.com>
2019-11-20 08:36:11 -0500

    f

--- pregunta3.py
+++ pregunta3.py
@@ -7,46 +7,51 @@ print("¡ CURSO DE NATACION !")
 alumnos = [a, b]
 #alumno 1
 a = int(input("Ingrese al primer alumno: "))
-sexo = int(input("Ingrese el sexo del alumno: "))
-if sexo is H or M:
+sexo1 = int(input("Ingrese el sexo del alumno: "))
+if sexo1 is H or M:
     pass
-edad = float(input("Ingrese edad del alumno: "))
+    print(sexo1)
+edad1 = float(input("Ingrese edad del alumno: "))
 
-if edad (>= 5 and <= 17):
-    print()
+if edad1 (>= 5 and <= 17):
+    print(edad1)
 
 #alumno 2
 b = int(input("Ingrese al segundo alumno: "))
-sexo = int(input("Ingrese el sexo del alumno: "))
-if sexo is H or M:
+sexo2 = int(input("Ingrese el sexo del alumno: "))
+if sexo2 is H or M:
     pass
-edad = int(input("Ingrese edad del alumno: "))
-if edad (>= 5 and <= 17):
-            print()
+    print(sexo2)
+edad2 = int(input("Ingrese edad del alumno: "))
+if edad2 (>= 5 and <= 17):
+    print(edad2)
 
 #Alumno 3
 b = int(input("Ingrese al tercer alumno: "))
-sexo = int(input("Ingrese el sexo del alumno: "))
-if sexo is H or M:
+sexo3 = int(input("Ingrese el sexo del alumno: "))
+if sexo3 is H or M:
     pass
-edad = int(input("Ingrese edad del alumno: "))
-if edad (>= 5 and <= 17):
-            print()
+    print(sexo3)
+edad3 = int(input("Ingrese edad del alumno: "))
+if edad3 (>= 5 and <= 17):
+    print(edad3)
 
 #Alumno 4
 b = int(input("Ingrese al cuarto alumno: "))
-sexo = int(input("Ingrese el sexo del alumno: "))
-if sexo is H or M:
+sexo4 = int(input("Ingrese el sexo del alumno: "))
+if sexo4 is H or M:
     pass
-edad = int(input("Ingrese edad del alumno: "))
-if edad (>= 5 and <= 17):
-            print()
+    print(sexo4)
+edad4 = int(input("Ingrese edad del alumno: "))
+if edad4 (>= 5 and <= 17):
+    print(edad4)
 
 #Alumno 5
 b = int(input("Ingrese al quinto alumno: "))
-sexo = int(input("Ingrese el sexo del alumno: "))
-if sexo is H or M:
+sexo5 = int(input("Ingrese el sexo del alumno: "))
+if sexo5 is H or M:
     pass
-edad = int(input("Ingrese edad del alumno: "))
-if edad (>= 5 and <= 17):
-            print()
+    print(sexo5)
+edad5 = int(input("Ingrese edad del alumno: "))
+if edad5 (>= 5 and <= 17):
+    print(edad5)
